c81c93dc03d3acf39cd5a549efc00c0bcbf11692
Nicolas De Jay <ndj@pinkfilter.org>
2015-03-03 06:08:20 -0500

    change names and paths to reflect the new pipeline standard

--- scripts/01-splitVideosIntoImages.sh
+++ scripts/01-splitVideosIntoImages.sh
@@ -2,11 +2,11 @@
 
 # for each avi file in the data, we create a similarly named directory (without
 # the avi file extension) and split out all the images there, in sequence.
-for f in data/*.avi ; do
+for f in ../data/input_videos/*.avi ; do
     echo "$f"
     p="$(basename "$f")"
-    d="data/images/$p"
+    d="../data/images/$p"
     mkdir -p "$d"
     # Specify the framerate and output file name format.
-    ffmpeg -i "$f" -r 30 -f image2 "$d/%4d.png"
+    ffmpeg -i "$f" -r 30 -f image2 "$d/static_%4d.png"
 done
