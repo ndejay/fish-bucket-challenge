40afe90f852d09e6d15157c8a6c78cf6d30693e5
wireless <wireless@tahiti.local>
2015-03-02 22:07:47 -0500

    Now is an importable module

--- scripts/produce_diff.py
+++ scripts/produce_diff.py
@@ -19,32 +19,28 @@ def to_grayscale(arr):
     else:
         return arr
 
-
-folder = sys.argv[1]
-
-
-# read images as 2D arrays (convert to grayscale for simplicity)
-onlyfiles = [ f for f in os.listdir(folder) if isfile(join(folder,f)) and f.endswith(".png") ]
-imgs = map( lambda x : to_grayscale(imread(x)).astype(float), [ os.path.join(folder ,  "%04d.png" % nb) for nb in range(1,len(onlyfiles)+1) ] )
-
-
-def compare_images(img1, img2):
-    diff = img1 - img2  # elementwise for scipy arrays
+def get_one_baseline(base_img, other_img):
+    diff = base_img - other_img  # elementwise for scipy arrays
     diff[diff <= 0 ] = 0
     return diff 
 
-diffs=[]
-for i in range(0,len(imgs)-1):
-    diffs.append( compare_images(imgs[0], imgs[i]))
-    imsave("../results_base/%s/out%04d.png" % (os.path.basename(folder),i)  ,diffs[-1])
-
-all_stuff = np.max(diffs, axis=0)
-all_stuff = all_stuff #/ np.max(all_stuff)
-print folder
+def get_all_baseline(base_img, other_imgs):
+    return map(lambda x : get_one_baseline(base_img, x), other_imgs )
 
 
-
-#imsave("../results/sum.png"  ,all_stuff)
+if __name__ == "__main__":
+    folder = sys.argv[1]
+    # read images as 2D arrays (convert to grayscale for simplicity)
+    onlyfiles = [ f for f in os.listdir(folder) if isfile(join(folder,f)) and f.endswith(".png") ]
+    imgs = map( lambda x : to_grayscale(imread(x)).astype(float), [ os.path.join(folder ,  "%04d.png" % nb) for nb in range(1,len(onlyfiles)+1) ] )
+    diffs = []
+    for i in range(0,len(imgs)-1):
+        diffs.append( get_one_baseline(imgs[0], imgs[i]))
+        imsave("../results_base/%s/out%04d.png" % (os.path.basename(folder),i)  ,diffs[-1])
+    all_stuff = np.max(diffs, axis=0)
+    all_stuff = all_stuff #/ np.max(all_stuff)
+    print folder
+    imsave("../results_base/%s/sum.png" % os.path.basename(folder) ,all_stuff)
 
 
 
