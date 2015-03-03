e9ca9ed0500a73a2107d708eec653c161913ffb6
Jacob Errington <nictytan@gmail.com>
2015-03-02 20:54:26 -0500

    add script to split videos into images

---
+++ scripts/makeimages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# for each avi file in the data, we create a similarly named directory (without
+# the avi file extension) and split out all the images there, in sequence.
+for f in data/*.avi ; do
+    echo "$f"
+    p="$(basename "$f")"
+    d="data/images/$p"
+    mkdir -p "$d"
+    # Specify the framerate and output file name format.
+    ffmpeg -i "$f" -r 30 -f image2 "$d/%4d.png"
+done
