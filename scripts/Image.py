fcd9a8720e6c00171923ec9618a04b84248d4146
Nicolas De Jay <ndj@pinkfilter.org>
2015-03-03 04:52:24 -0500

    finalized image class

--- scripts/Image.py
+++ scripts/Image.py
@@ -2,35 +2,40 @@ from skimage.color import rgb2gray
 from skimage.io import imread
 from skimage.io import imsave
 from skimage.feature import blob_dog
+from math import sqrt
 import matplotlib.pyplot as plt
 import numpy as np
 
 class Image(object):
-    img = []
+    img   = []
     blobs = []
 
-    def __init__(self, img, blobs = None):
+    def __init__(self, img = [], blobs = None):
         self.img = img
         if blobs != None:
             self.blobs = blobs
 
     def load(self, filename):
         self.img = imread(filename)
+        return self
 
     def saveImage(self, filename):
         imsave(filename, self.img)
+        return self
 
     def saveFig(self, filename):
         fig, ax = plt.subplots(1, 1)
-        ax.imshow(self.image)
-        for blob in blobs:
+        ax.imshow(self.img)
+        for blob in self.blobs:
             y, x, r = blob
             c = plt.Circle((x, y), r, linewidth = 2, color = 'red')
             ax.add_patch(c)
         fig.savefig(filename)
+        return self
 
     def saveBlobs(self, filename):
-	    np.savetxt(filename, self.blobs, delimiter = ",")
+        np.savetxt(filename, self.blobs, delimiter = ",")
+        return self
 
     def gray(self):
         return Image(rgb2gray(self.img))
@@ -39,11 +44,14 @@ class Image(object):
         return Image(1 - self.img)
 
     def blobs(self, max_sigma, threshold):
-        blobs = blob_dog(self.gray, max_sigma = max_sigma, threshold = threshold)
+        blobs = blob_dog(self.gray().img, max_sigma = max_sigma, threshold = threshold)
         blobs[:, 2] = blobs[:, 2] * sqrt(2)
-        self.blobs = blobs
+        return Image(self.img, blobs)
 
     def __sub__(self, image):
-        return Image(self.img - image.image)
+        return Image(self.img- image.img)
+
+    def __add__(Self, image):
+        return Image((self.img+ image.img) / 2)
 
 # vim: set ts=4 sw=4 sts=4 et :
